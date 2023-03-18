384a3460ef6fb9b37f68d4925e3761de747d7596
Joey Huab <joey@evolution-x.org>
2023-03-18 03:02:43 +0000

    [SQUASH] base: Target Pixel Launcher/Pixel themes again
    Revert "SystemUI: Set launcher activity name to Launcher3"
    
    This reverts commit 5c0b87017ea9acc4c9d01a8b6dc2eafc51cf0726.
    
    Revert "SystemUI: Support smartspace animation for pixel launcher"
    
    This reverts commit 4f3bae44f4e481b1e0bed82fa8e98a1ac7bc4b69.
    
    Revert "Revert "overlay: Target pixel launcher/pixel themes""
    
    This reverts commit e465483e67358fff9a42054fbf2d3cf5204f5d0b.

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardUnlockAnimationController.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardUnlockAnimationController.kt
@@ -987,11 +987,9 @@ class KeyguardUnlockAnimationController @Inject constructor(
          * transitions!
          */
         fun isNexusLauncherUnderneath(): Boolean {
-            return when (ActivityManagerWrapper.getInstance().runningTask?.topActivity?.className) {
-                QuickStepContract.LAUNCHER_ACTIVITY_CLASS_NAME,
-                QuickStepContract.NEXUS_LAUNCHER_ACTIVITY_CLASS_NAME -> true
-                else -> false
-            }
+            return ActivityManagerWrapper.getInstance()
+                    .runningTask?.topActivity?.className?.equals(
+                            QuickStepContract.LAUNCHER_ACTIVITY_CLASS_NAME) ?: false
         }
 
         fun isFoldable(context: Context): Boolean {
